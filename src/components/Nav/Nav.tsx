fae5425bde797a0956c960fe92cf09b310cc0896
israelsanttana <israelsantanasantos7@gmail.com>
2024-03-16 14:19:21 -0300

    fix: ajsute de tamanho no mobile da nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -6,15 +6,15 @@ export function Nav() {
     return (
         <div className="w-full pt-4">
             <div className="container mx-auto">
-                <div className="flex w-full justify-between h-24">
-                    <div className="self-center">
+                <div className="flex w-full justify-between items-center h-20 lg:h-24">
+                    <div className="self-end lg:self-center">
                         <h2>
-                            <img src={logo} alt="" width={80} className="w-20 md:w-24 lg:w-28" />
+                            <img src={logo} alt="" width={80} className="w-18 lg:w-24" />
                             <p className="text-slate-900 pl-6 font-bold -mt-8">INSTITUIÇÃO</p>
                         </h2>
                     </div>
-                    <div className="self-center">
-                        <Button className="text-slate-900">FALAR COM UM CONSULTOR</Button>
+                    <div className="self-end lg:self-center">
+                        <Button className="text-sm">FALAR COM UM CONSULTOR</Button>
                     </div>
                 </div>
             </div>
