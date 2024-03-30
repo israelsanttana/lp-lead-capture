155ca901d0927453b7001e331b5ea0effa3e660e
israelsanttana <israelsantanasantos7@gmail.com>
2024-03-30 11:35:33 -0300

    fix: ajuste de imagem e texto

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import img from '../../assets/imgHero.png'
+import img from '../../assets/hero.png'
 import { Form } from '../Form/Form'
 
 export function Hero() {
@@ -7,9 +7,13 @@ export function Hero() {
             <div className="container mx-auto mt-4 pt-8 pb-8 lg:mt-4 mb-4 lg:pt-0 lg:pb-0">
 
                 <div className="flex flex-col lg:flex-row">
-
-                    <div className="flex flex-col gap-4 flex-1 self-center ">
-                        <h1 className="text-5xl font-bold text-white text-center lg:text-7xl lg:text-left">
+                    <div className='flex-1'>
+                        <div className=" sm:w-full hidden lg:block">
+                            <img src={img} alt="foto de uma engenheira e um homem em momento de formação academica" />
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-4 flex-1 justify-center lg:items-end">
+                        <h1 className="text-5xl font-bold text-white text-center lg:text-7xl lg:text-right">
                             DISCIPLINAS ISOLADAS
                         </h1>
                         <h3 className="text-2xl  text-white text-center lg:text-2xl lg:text-left">
@@ -18,11 +22,6 @@ export function Hero() {
                         </h3>
                         <div className='lg:w-[500px]'>
                             <Form />
-                        </div>
-                    </div>
-                    <div className='flex-1'>
-                        <div className=" sm:w-full hidden lg:block">
-                            <img src={img} alt="foto de uma engenheira e um homem em momento de formação academica" />
                         </div>
                     </div>
                 </div>
