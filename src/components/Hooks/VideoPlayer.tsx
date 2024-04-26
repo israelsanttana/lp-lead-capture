f661e2f129c58503eb0ebc1846ba09e20311f829
israelsanttana <israelsantanasantos7@gmail.com>
2024-04-25 23:24:53 -0300

    fix: ajusta importação

--- src/components/Hooks/VideoPlayer.tsx
+++ src/components/Hooks/VideoPlayer.tsx
@@ -3,7 +3,7 @@ import {
     DialogContent,
     DialogTrigger,
 } from "@/components/ui/dialog";
-import video from "../../assets/Vídeo-Sérgio-Pacheco_v3 (1).mp4";
+// import video from "../../assets/Vídeo-Sérgio-Pacheco_v3 (1).mp4";
 import capaVideo from "../../assets/capa-video.png";
 
 export const VideoPlayer = () => {
@@ -11,7 +11,7 @@ export const VideoPlayer = () => {
         <Dialog>
             <DialogTrigger><img src={capaVideo} width={480} alt="Capa do Vídeo" /></DialogTrigger>
             <DialogContent className="bg-transparent text-white border-none shadow-none">
-                <video className="w-full h-full p-3" src={video} controls autoPlay title="VideoPlayer"></video>
+                {/* <video className="w-full h-full p-3" src={video} controls autoPlay title="VideoPlayer"></video> */}
             </DialogContent>
         </Dialog>
 
