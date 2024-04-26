01cc16ae96d996a7cd67cd5e27052abfade9e106
israelsanttana <israelsantanasantos7@gmail.com>
2024-04-26 02:12:15 -0300

    fix: ajusta overflow no mobile

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -16,34 +16,7 @@ const testemunhos = [
         texto: "Gostaria de compartilhar a minha gratidão pelo ótimo suporte e consultoria que recebi da empresa. Ao precisar realizar disciplinas isoladas para a conclusão da minha graduação, fui muito feliz pelo atendimento impecável que me foi prestado. Todas as disciplinas foram realizadas de acordo com as diretrizes da minha instituição, possibilitando o aproveitamento necessário para a conclusão do meu curso. Agradeço profundamente pela consultoria precisa, que me orientou exatamente no que eu buscava. Nem sempre o bom é notado, mas quando se destaca, merece ser compartilhado. Obrigada.",
         curso: "Nome do Curso 1",
     },
-    {
-        id: 2,
-        nome: "Nome do Testemunho 2",
-        imagem: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
-        texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua fefef fefe fefef efefef fefefe fefefe fefef efefefefef efef frfergg rgregreg ergregerggregergregregregergregerg.",
-        curso: "Nome do Curso 2",
-    },
-    {
-        id: 3,
-        nome: "Nome do Testemunho 3",
-        imagem: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
-        texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        curso: "Nome do Curso 3",
-    },
-    {
-        id: 4,
-        nome: "Nome do Testemunho 4",
-        imagem: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
-        texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        curso: "Nome do Curso 4",
-    },
-    {
-        id: 5,
-        nome: "Nome do Testemunho 5",
-        imagem: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
-        texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        curso: "Nome do Curso 5",
-    },
+
 ];
 
 export function CarouselLayout() {
@@ -59,9 +32,9 @@ export function CarouselLayout() {
                 <Carousel opts={{ align: "start" }} className="w-full max-w-6xl ">
                     <CarouselContent>
                         {testemunhos.map((testemunho) => (
-                            <CarouselItem key={testemunho.id} className="md:basis-1/2 lg:basis-1/3">
+                            <CarouselItem key={testemunho.id} className=" md:basis-1/2 lg:basis-1/3">
                                 <div className="p-1">
-                                    <Card>
+                                    <Card className="h-[360px] md:h-full">
                                         <CardContent className="flex flex-col items-center justify-between p-6 md:min-h-[300px] drop-shadow-xl ">
                                             <div className="grid grid-cols-[100px_2fr]">
                                                 <div className="flex items-center">
@@ -72,7 +45,7 @@ export function CarouselLayout() {
                                                     <p className="text-gray-500">{testemunho.curso}</p>
                                                 </div>
                                             </div>
-                                            <div className="overflow-auto overflow-x-hidden max-h-[100px] lg:max-h-[110px] md:max-h-[100px] mb-2 p-1">
+                                            <div className="overflow-auto overflow-x-hidden max-h-[200px] lg:max-h-[110px] md:max-h-[100px] mb-2 p-1">
                                                 <p className="text-gray-600 text-center">{testemunho.texto}</p>
                                             </div>
                                             <div>
@@ -84,8 +57,8 @@ export function CarouselLayout() {
                             </CarouselItem>
                         ))}
                     </CarouselContent>
-                    <CarouselPrevious />
-                    <CarouselNext />
+                    <CarouselPrevious className="hidden md:block" />
+                    <CarouselNext className="hidden md:block" />
                 </Carousel>
             </div>
         </div>
